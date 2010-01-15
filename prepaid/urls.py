3599ec52800886cc75ac6199fc6df0e839505720
CrowdSense <admin@DevBook.home>
2010-01-15 11:53:53 +0000

    pure backup

--- prepaid/urls.py
+++ prepaid/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('prepaid.views',
     (r'^$', 'points', {}, 'prepaid-index'),
+    (r'^account-summary/$', 'points', {}, 'prepaid-account-summary'),
     (r'^withdraw/$', 'withdraw', {}, 'prepaid-withdraw'),
 )
